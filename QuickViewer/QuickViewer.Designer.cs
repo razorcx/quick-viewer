815a0a0534882371b1909f5ecad09bc4d96288bc
Cory LePage <cory@razorcx.com>
2017-11-26 18:11:09 -0800

    Initial commit

---
+++ QuickViewer/QuickViewer.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace QuickViewer
+{
+	partial class QuickViewer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuickViewer));
+			this.dataGridMembers = new Tekla.Structures.Dialog.UIControls.DataGrid();
+			this.labelDataGrid = new System.Windows.Forms.Label();
+			this.btnRefresh = new System.Windows.Forms.Button();
+			this.cmbBoxMember = new System.Windows.Forms.ComboBox();
+			this.logo = new System.Windows.Forms.PictureBox();
+			this.txtBoxSearch = new System.Windows.Forms.TextBox();
+			this.lblSearch = new System.Windows.Forms.Label();
+			this.txtBoxStatus = new System.Windows.Forms.TextBox();
+			this.txtBoxModelInfo = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridMembers)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridMembers
+			// 
+			this.dataGridMembers.AllowUserToAddRows = false;
+			this.dataGridMembers.AllowUserToDeleteRows = false;
+			this.dataGridMembers.AllowUserToOrderColumns = true;
+			this.dataGridMembers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridMembers.BackgroundColor = System.Drawing.Color.White;
+			this.dataGridMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridMembers.GridColor = System.Drawing.SystemColors.ControlLight;
+			this.dataGridMembers.Location = new System.Drawing.Point(12, 150);
+			this.dataGridMembers.Name = "dataGridMembers";
+			this.dataGridMembers.ReadOnly = true;
+			this.dataGridMembers.RowTemplate.Height = 24;
+			this.dataGridMembers.Size = new System.Drawing.Size(750, 429);
+			this.dataGridMembers.TabIndex = 1;
+			this.dataGridMembers.SelectionChanged += new System.EventHandler(this.dataGridMembers_SelectionChanged);
+			// 
+			// labelDataGrid
+			// 
+			this.labelDataGrid.AutoSize = true;
+			this.labelDataGrid.Location = new System.Drawing.Point(12, 100);
+			this.labelDataGrid.Name = "labelDataGrid";
+			this.labelDataGrid.Size = new System.Drawing.Size(100, 17);
+			this.labelDataGrid.TabIndex = 2;
+			this.labelDataGrid.Text = "Member Name";
+			// 
+			// btnRefresh
+			// 
+			this.btnRefresh.Location = new System.Drawing.Point(657, 113);
+			this.btnRefresh.Name = "btnRefresh";
+			this.btnRefresh.Size = new System.Drawing.Size(105, 33);
+			this.btnRefresh.TabIndex = 5;
+			this.btnRefresh.Text = "Refresh";
+			this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+			// 
+			// cmbBoxMember
+			// 
+			this.cmbBoxMember.FormattingEnabled = true;
+			this.cmbBoxMember.Location = new System.Drawing.Point(12, 120);
+			this.cmbBoxMember.Name = "cmbBoxMember";
+			this.cmbBoxMember.Size = new System.Drawing.Size(206, 24);
+			this.cmbBoxMember.TabIndex = 4;
+			this.cmbBoxMember.SelectedIndexChanged += new System.EventHandler(this.cmbBoxMember_SelectedIndexChanged);
+			// 
+			// logo
+			// 
+			this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+			this.logo.Location = new System.Drawing.Point(274, 19);
+			this.logo.Margin = new System.Windows.Forms.Padding(10);
+			this.logo.Name = "logo";
+			this.logo.Padding = new System.Windows.Forms.Padding(20);
+			this.logo.Size = new System.Drawing.Size(256, 55);
+			this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.logo.TabIndex = 5;
+			this.logo.TabStop = false;
+			this.logo.Click += new System.EventHandler(this.logo_Click);
+			// 
+			// txtBoxSearch
+			// 
+			this.txtBoxSearch.Location = new System.Drawing.Point(274, 122);
+			this.txtBoxSearch.Name = "txtBoxSearch";
+			this.txtBoxSearch.Size = new System.Drawing.Size(256, 22);
+			this.txtBoxSearch.TabIndex = 6;
+			this.txtBoxSearch.TextChanged += new System.EventHandler(this.txtBoxSearch_TextChanged);
+			// 
+			// lblSearch
+			// 
+			this.lblSearch.AutoSize = true;
+			this.lblSearch.Location = new System.Drawing.Point(274, 101);
+			this.lblSearch.Name = "lblSearch";
+			this.lblSearch.Size = new System.Drawing.Size(152, 17);
+			this.lblSearch.TabIndex = 7;
+			this.lblSearch.Text = "Search Assembly Mark";
+			// 
+			// txtBoxStatus
+			// 
+			this.txtBoxStatus.Location = new System.Drawing.Point(66, 588);
+			this.txtBoxStatus.Name = "txtBoxStatus";
+			this.txtBoxStatus.Size = new System.Drawing.Size(294, 22);
+			this.txtBoxStatus.TabIndex = 8;
+			// 
+			// txtBoxModelInfo
+			// 
+			this.txtBoxModelInfo.Location = new System.Drawing.Point(471, 588);
+			this.txtBoxModelInfo.Name = "txtBoxModelInfo";
+			this.txtBoxModelInfo.Size = new System.Drawing.Size(291, 22);
+			this.txtBoxModelInfo.TabIndex = 9;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 591);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 17);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Status";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(378, 591);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(87, 17);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Model Name";
+			// 
+			// QuickViewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(774, 622);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtBoxModelInfo);
+			this.Controls.Add(this.txtBoxStatus);
+			this.Controls.Add(this.lblSearch);
+			this.Controls.Add(this.txtBoxSearch);
+			this.Controls.Add(this.cmbBoxMember);
+			this.Controls.Add(this.btnRefresh);
+			this.Controls.Add(this.logo);
+			this.Controls.Add(this.labelDataGrid);
+			this.Controls.Add(this.dataGridMembers);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "QuickViewer";
+			this.Text = "Quick Viewer";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridMembers)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private Tekla.Structures.Dialog.UIControls.DataGrid dataGridMembers;
+		private System.Windows.Forms.Label labelDataGrid;
+		private System.Windows.Forms.Button btnRefresh;
+		private System.Windows.Forms.ComboBox cmbBoxMember;
+		private System.Windows.Forms.PictureBox logo;
+		private System.Windows.Forms.TextBox txtBoxSearch;
+		private System.Windows.Forms.Label lblSearch;
+		private System.Windows.Forms.TextBox txtBoxStatus;
+		private System.Windows.Forms.TextBox txtBoxModelInfo;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ QuickViewer/QuickViewer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using TSDUI = Tekla.Structures.Dialog.UIControls;
+
+namespace QuickViewer
+{
+	public partial class QuickViewer : Form
+	{
+		private readonly Model _model = new Model();
+		private List<dynamic> _data;
+
+		public QuickViewer()
+		{
+			try
+			{
+				InitializeComponent();
+				this.TopMost = true;
+				this.cmbBoxMember.DataSource = GetBeamNames();
+				this.txtBoxModelInfo.Text = _model.GetInfo().ModelName;
+				UpdateStatus();
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+				throw;
+			}
+		}
+
+		private void UpdateStatus()
+		{
+			var message = $"Diplaying: {this.cmbBoxMember.Text} ({_data.Count})";
+			this.txtBoxStatus.Text = message;
+		}
+
+		private void btnRefresh_Click(object sender, EventArgs e)
+		{
+			var beams = GetBeams();
+			GetData(beams);
+			this.txtBoxSearch.Text = string.Empty;
+			this.dataGridMembers.DataSource = _data;
+			UpdateStatus();
+		}
+
+		private void cmbBoxMember_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var beams = GetBeams();
+			GetData(beams);
+			this.txtBoxSearch.Text = string.Empty;
+			this.dataGridMembers.DataSource = _data;
+			UpdateStatus();
+		}
+
+		private List<string> GetBeamNames()
+		{
+			var beams =
+				GetBeams()
+				.Select(b => b.Name)
+				.Distinct()
+				.OrderBy(b => b)
+				.ToList();
+
+			beams.Insert(0, "ALL");
+			return beams;
+		}
+
+		private void GetData(List<Beam> beams)
+		{
+			if (!(this.cmbBoxMember.Text == "ALL"))
+				beams = beams.Where(b => b.Name == this.cmbBoxMember.Text).ToList();
+
+			_data = beams
+				.Select(b =>
+				{
+					var phase = -1;
+					b.GetReportProperty("PHASE", ref phase);
+
+					var assemblyPos = string.Empty;
+					b.GetReportProperty("ASSEMBLY_POS", ref assemblyPos);
+
+					return new
+					{
+						Id = b.Identifier.ID,
+						Phase = phase,
+						Assembly_Mark = assemblyPos,
+						Name = b.Name,
+						Profile = b.Profile.ProfileString,
+						Class = b.Class,
+						Finish = b.Finish,
+						Material = b.Material.MaterialString,
+					};
+				})
+				.OrderBy(b => b.Phase)
+				.ThenBy(b => b.Name)
+				.ThenBy(b => b.Profile)
+				.ToList<dynamic>();
+		}
+
+		private List<Beam> GetBeams()
+		{
+			ModelObjectEnumerator.AutoFetch = true;
+			var moe = _model.GetModelObjectSelector().GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM);
+			return 
+				moe
+				.ToList()
+				.OfType<Beam>()
+				.ToList();
+		}
+
+		private void SelectModelObjectsInUi(List<int> ids)
+		{
+			var modelObjects = new ArrayList();
+
+			ids.ForEach(id => 
+			{
+				var modelObject = _model.SelectModelObject(new Identifier(id));
+				if (modelObject == null) return;
+				modelObjects.Add(modelObject);
+			});
+
+			var selector = new Tekla.Structures.Model.UI.ModelObjectSelector();
+			selector.Select(modelObjects);
+		}
+
+		private void txtBoxSearch_TextChanged(object sender, EventArgs e)
+		{
+			var searchText = ((TextBox) sender).Text;
+			if (string.IsNullOrEmpty(searchText)) return;
+
+			var beams = GetBeams();
+			if (!(this.cmbBoxMember.Text == "ALL"))
+				beams = beams.Where(b => b.Name == this.cmbBoxMember.Text).ToList();
+
+			beams = beams
+				.Where(b =>
+				{
+					var assemblyPos = string.Empty;
+					b.GetReportProperty("ASSEMBLY_POS", ref assemblyPos);
+
+					return assemblyPos.Contains(searchText);
+				})
+				.ToList();
+
+			GetData(beams);
+			this.dataGridMembers.DataSource = _data;
+			UpdateStatus();
+		}
+
+		private void dataGridMembers_SelectionChanged(object sender, EventArgs e)
+		{
+			var selectedRows = GetSelectedRows(sender);
+			var ids = GetIds(selectedRows);
+			SelectModelObjectsInUi(ids);
+		}
+
+		private List<DataGridViewRow> GetSelectedRows(object sender)
+		{
+			var dataGrid = sender as TSDUI.DataGrid;
+			return dataGrid?.SelectedRows.OfType<DataGridViewRow>().ToList();
+		}
+
+		private List<int> GetIds(List<DataGridViewRow> rows)
+		{
+			return rows
+				.Select(r => (int)((dynamic)r.DataBoundItem).Id)
+				.ToList();
+		}
+
+		private void logo_Click(object sender, EventArgs e)
+		{
+			Process.Start("http://razorcx.com");
+		}
+	}
+}
